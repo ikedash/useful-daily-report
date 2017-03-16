0870d90ce4157555b4b7b4ab13a0fda4e99796a5
ikeshin <ikeda.shinya.510@tis.co.jp>
2017-03-16 17:11:50 +0900

    update Formの利用

--- src/main/java/jp/co/tis/rookies/controller/DailyReportController.java
+++ src/main/java/jp/co/tis/rookies/controller/DailyReportController.java
@@ -1,9 +1,13 @@
 package jp.co.tis.rookies.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +19,11 @@ import jp.co.tis.rookies.service.DailyReportService;
 public class DailyReportController {
     @Autowired
     DailyReportService service;
+
+    @ModelAttribute
+    DailyReportForm setUpForm() {
+        return new DailyReportForm();
+    }
 
     /**
      * 日報入力画面.
@@ -31,9 +40,15 @@ public class DailyReportController {
      * 投稿内容確認画面.
      */
     @RequestMapping(value = "/daily-report/confirm", method = RequestMethod.POST)
-    public String confirm(Model model, @RequestParam("title") String title, @RequestParam("body") String body) {
-        model.addAttribute("title", title);
-        model.addAttribute("body", body);
+    public String confirm(@Validated DailyReportForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            // 暫定的なエラー処理.
+            setErrors(result, model);
+            return "input";
+        }
+
+        model.addAttribute("title", form.getTitle());
+        model.addAttribute("body", form.getBody());
 
         return "confirm";
     }
@@ -66,13 +81,27 @@ public class DailyReportController {
      * @return success.jsp
      */
     @RequestMapping(value = "/daily-report/post", method = RequestMethod.POST)
-    public String post(Model model, @RequestParam("title") String title, @RequestParam("body") String body) {
+    public String post(@Validated DailyReportForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            // 暫定的なエラー処理.
+            setErrors(result, model);
+            return "input";
+        }
+
         DailyReport report = new DailyReport();
-        report.setTitle(title);
-        report.setBody(body);
+        BeanUtils.copyProperties(form, report);
 
         service.create(report);
 
         return "success";
+    }
+
+    private void setErrors(BindingResult result, Model model) {
+        if (result.hasFieldErrors("title")) {
+            model.addAttribute("title", "精査エラー");
+        }
+        if (result.hasFieldErrors("body")) {
+            model.addAttribute("body", "精査エラー");
+        }
     }
 }
